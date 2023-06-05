85828821d2435c985e2bfba9873df5eea46c24e4
KamilMoczydlowski <kamil.moczydlowski@icloud.com>
2023-06-05 20:34:32 +0200

    comments added

--- calc.js
+++ calc.js
@@ -1,27 +1,32 @@
 function getFinalResult() {
 	const pairs = [90, 10, 1, 2, 3, 4, 5, 10, 1, 90, 90, 90, 10, 22];
-	let arrOfCheckedValues = [];
-	let arrOfObjects = [];
-	let finalResult;
+	let arrOfCheckedValues = []; // wrzucamy tu wartości bez dubli
+	let arrOfObjects = []; // wrzucamy tu object {id, value} bez dubli
+	let finalResult; // ostateczny wynik, liczba
 
 	for (let i = 0; i < pairs.length; i++) {
 
 		if (arrOfCheckedValues.indexOf(pairs[i]) === -1) {
-			const value = pairs.filter(item => item === pairs[i]);
+			// filtrujemy wskazaną wartość na tablicy
+			const value = pairs.filter(item => item === pairs[i]); 
 
-			arrOfObjects.push({ id:pairs[i], value: value.length });
+			// wrzucamy na nową tablicę objekt bez dubli
+			arrOfObjects.push({ id:pairs[i], value: value.length }); 
 
-			arrOfCheckedValues.push(pairs[i]);
+			// wrzucamy wartość którą już sprawdziliśmy przefiltrowaliśmy
+			arrOfCheckedValues.push(pairs[i]); 
 		}
 	}
 
-    
+	// sumujemy liczby powtórzeń
     finalResult = arrOfObjects.reduce((acc, obj) => {
-        return acc+obj.value
+        return acc + obj.value
     }, 0)
 
+	// zwracamy końcowy wynik
 	return finalResult;
 }
 
+// przypisujemy wartość do stałej
 const answer = getFinalResult();
-console.log(answer);
+console.log(answer); // logujemy wynik - powinno wyjść 14
